a796f2575d53e942652fc1ca021d99069d49e835
Satsuki <satsuki200214@gmail.com>
2024-12-09 22:41:21 +0900

    [add]KioskRegistration画面を追加しました

---
+++ src/KioskHub.Wpf/ViewModels/KioskRegistrationPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KioskHub.Wpf.ViewModels
+{
+    public class KioskRegistrationPageViewModel
+    {
+        public ReactivePropertySlim<string> KioskAppPath { get; } = new();
+
+        public ReactiveCommandSlim KioskOnCommand { get; } = new();
+    }
+}
