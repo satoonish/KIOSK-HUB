f3dd85b63c3d0e32caf3fd6c7f50389e53ff1ce0
Satsuki <satsuki200214@gmail.com>
2024-12-10 23:11:43 +0900

    [add]MainWindowにFrameを配置して画面を切り替えられるようにしました

--- src/KioskHub.Wpf/ViewModels/MainWindowViewModel.cs
+++ src/KioskHub.Wpf/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using KioskHub.Wpf.Views;
+using Reactive.Bindings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace KioskHub.Wpf.ViewModels
 {
     public class MainWindowViewModel
     {
+        public ReactiveProperty<Page> FrameSource { get; } = new();
+
+        public MainWindowViewModel()
+        {
+            FrameSource.Value = new KioskRegistrationPage();
+        }
     }
 }
